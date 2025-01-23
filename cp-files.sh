f6694e649252c78de0557b5833b951b907f3a01d
stundrg <khr0106@naver.com>
2025-01-23 10:33:55 +0900

    docs

--- cp-files.sh
+++ cp-files.sh
@@ -5,6 +5,6 @@ date
 timedatectl
 cp CNAME docs/
 cp robots.txt docs/
-cp naverdb736232788ff4342108ef3c15b188a4.html
-cp google7a88b1c299e14b23.html
+cp naverdb736232788ff4342108ef3c15b188a4.html docs/
+cp google7a88b1c299e14b23.html docs/
 cat /etc/os-release
